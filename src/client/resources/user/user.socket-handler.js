bd0176a120b1fb62c99c7b589a76ce7cbae39c34
Nikita Nesterenko <nesterenko.nikita.sergeevich@gmail.com>
2019-12-09 17:06:59 +0300

    edit socket handler connect. allow to subscribe to sockets in components

--- src/client/resources/user/user.socket-handler.js
+++ src/client/resources/user/user.socket-handler.js
@@ -1,10 +1,15 @@
 import store from 'resources/store';
+
+import * as fromUser from './user.selectors';
 import {
   UPDATE_CURRENT_USER,
-} from 'resources/user/user.actions-list';
+} from './user.actions-list';
 
 
 export default (socket) => {
+  const userId = fromUser.getUserId(store.getState());
+  socket.emit('subscribe', `user-${userId}`);
+
   socket.on('user:updated', (user) => {
     store.dispatch({ type: UPDATE_CURRENT_USER, payload: user });
   });
--- src/client/services/socket.service.js
+++ src/client/services/socket.service.js
@@ -1,27 +1,28 @@
 import io from 'socket.io-client';
 
 import config from 'config';
-import store from 'resources/store';
 import { getAccessToken } from 'helpers/token.helper';
-import * as fromUser from 'resources/user/user.selectors';
 import userHandler from 'resources/user/user.socket-handler';
 
 
+let socket; // eslint-disable-line import/no-mutable-exports
+
 export const connect = () => {
-  const socket = io(config.webSocketUrl, {
+  socket = io(config.webSocketUrl, {
     transports: ['websocket'],
     query: { accessToken: getAccessToken() },
   });
 
   socket.on('connect', () => {
     console.log('WS connected'); // eslint-disable-line no-console
-    const userId = fromUser.getUserId(store.getState());
-    socket.emit('subscribe', `user-${userId}`);
+    userHandler(socket);
   });
 
   socket.on('disconnect', () => {
     console.log('WS disconnected'); // eslint-disable-line no-console
   });
+};
 
-  userHandler(socket);
+export {
+  socket,
 };
