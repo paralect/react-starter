0a5eff76e51970d68cc23fc4fdc039179b1f919a
Gloria Loi <loigloria2001@gmail.com>
2021-09-03 11:23:33 +0300

    Add useFetch hook

---
+++ src/hooks/useFetch.js
@@ -0,0 +1,40 @@
+import { useEffect, useReducer } from 'react';
+
+const reducerFunc = (prevState, newState) => ({
+  ...prevState,
+  ...newState,
+});
+
+const initialReducerState = {
+  data: null,
+  loading: true,
+  error: null,
+};
+
+export const useFetch = (fetchFunc, { onFinish, params }) => {
+  const [{ data, loading, error }, setState] = useReducer(reducerFunc, initialReducerState);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        setState({ loading: true });
+        let response;
+        if (typeof fetchFunc === 'function') {
+          response = await fetchFunc(params);
+        } else {
+          response = await Promise.all(fetchFunc);
+        }
+        setState({ data: response, loading: false });
+      } catch (err) {
+        setState({ error: err, loading: false });
+      } finally {
+        if (onFinish) onFinish();
+      }
+    };
+
+    fetchData();
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [params]);
+
+  return { data, loading, error };
+};
