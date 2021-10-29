e1ca6bb552327d4417ab2b7f8896d2d26660bd5e
Fruneen <fruneen@gmail.com>
2021-10-29 14:54:16 +0300

    feat add logic for segmented control component

--- src/components/SegmentedControl/SegmentedControl.jsx
+++ src/components/SegmentedControl/SegmentedControl.jsx
@@ -1,25 +1,81 @@
-import React, { memo } from 'react';
+import React, {
+  memo, useCallback, useEffect, useLayoutEffect, useRef, useState,
+} from 'react';
 import cn from 'classnames';
 
+import PropTypes from 'prop-types';
 import styles from './SegmentedControl.pcss';
 
 const SegmentedControl = ({
-  // eslint-disable-next-line
+  // eslint-disable-next-line react/prop-types
   value, onChange, options,
 }) => {
+  const containerRef = useRef(null);
+  const segmentRef = useRef(null);
+
+  const [segmentedControls, setSegmentedControls] = useState(options);
+  const [segmentWidth, setSegmentWidth] = useState(0);
+
+  useLayoutEffect(() => {
+    document.onreadystatechange = () => setSegmentWidth(containerRef.current?.offsetWidth);
+  }, [containerRef.current?.offsetWidth]);
+
+  useEffect(() => {
+    const indexOfActiveSegment = options.findIndex((option) => option.value === value.value);
+
+    setSegmentedControls(options.map((option) => {
+      const newOption = { ...option };
+      newOption.isActive = option.value === value.value;
+      return newOption;
+    }));
+
+    segmentRef.current.style.transform = `translateX(${
+      (containerRef.current?.offsetWidth / options.length) * indexOfActiveSegment
+      // (containerWidth / countOfSegments) * indexOfActiveSegment
+    }px)`;
+  }, [value.value, options, segmentWidth]);
+
+  const handleClick = useCallback((index) => onChange(options[index]), [onChange, options]);
+
   return (
     <div className={styles.container}>
-      {/* eslint-disable-next-line react/prop-types */}
-      {options.map((option) => (
-        <div className={cn({
-          [styles.active]: option.isActive,
-        }, styles.segment)}
-        >
-          {option.label}
-        </div>
-      ))}
+      <div className={styles.innerContainer} ref={containerRef}>
+        <div
+          className={styles.segment_mask}
+          style={{ width: `calc((100% / ${options.length}) - 2px)` }}
+          ref={segmentRef}
+        />
+        {segmentedControls.map((option, index) => (
+          // eslint-disable-next-line
+          <div
+            key={option.value}
+            className={cn({
+              [styles.active]: option.isActive,
+            }, styles.segment)}
+            onClick={() => handleClick(index)}
+          >
+            {option.label}
+          </div>
+        ))}
+      </div>
     </div>
   );
+};
+
+SegmentedControl.propTypes = {
+  options: PropTypes.arrayOf(PropTypes.shape({
+    label: PropTypes.string,
+    value: PropTypes.string,
+  })),
+  value: PropTypes.shape({
+    label: PropTypes.string,
+    value: PropTypes.string,
+  }),
+};
+
+SegmentedControl.defaultProps = {
+  options: null,
+  value: null,
 };
 
 export default memo(SegmentedControl);
--- src/components/SegmentedControl/SegmentedControl.stories.jsx
+++ src/components/SegmentedControl/SegmentedControl.stories.jsx
@@ -4,17 +4,24 @@ import SegmentedControl from './SegmentedControl';
 
 const options = [
   {
-    label: 'Option',
+    label: 'Option 1',
     value: 'option-1',
   },
   {
-    label: 'Option',
+    label: 'Option 2',
     value: 'option-2',
-    isActive: true,
   },
   {
-    label: 'Option',
+    label: 'Option 3',
     value: 'option-3',
+  },
+  {
+    label: 'Option 4',
+    value: 'option-4',
+  },
+  {
+    label: 'Option 5',
+    value: 'option-5',
   },
 ];
 
