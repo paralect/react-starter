d7efc5b9de7f16cc66f7db5e48bff8ad94052a70
Fruneen <fruneen@gmail.com>
2021-11-08 15:47:48 +0300

    feat: add spinner component

--- src/components/Spinner/Spinner.jsx
+++ src/components/Spinner/Spinner.jsx
@@ -4,6 +4,7 @@ import PropTypes from 'prop-types';
 
 import styles from './Spinner.pcss';
 
+<<<<<<< HEAD
 const sizes = {
   l: 'l',
   m: 'm',
@@ -13,6 +14,11 @@ const sizes = {
 const Spinner = ({ theme, size }) => {
   return (
     <div className={cn(styles[size], styles.container)}>
+=======
+const Spinner = ({ theme }) => {
+  return (
+    <div className={styles.container}>
+>>>>>>> feat: add spinner component
       <svg className={cn({
         [styles.dark]: theme === 'dark',
       }, styles.spinner, styles.spinner_loading)}
@@ -31,12 +37,18 @@ const Spinner = ({ theme, size }) => {
 
 Spinner.propTypes = {
   theme: PropTypes.oneOf(['dark', 'light']),
+<<<<<<< HEAD
   size: PropTypes.oneOf(Object.values(sizes)),
+=======
+>>>>>>> feat: add spinner component
 };
 
 Spinner.defaultProps = {
   theme: 'light',
+<<<<<<< HEAD
   size: sizes.m,
+=======
+>>>>>>> feat: add spinner component
 };
 
 export default memo(Spinner);
